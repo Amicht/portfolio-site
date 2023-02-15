b03bb2c789157ccba22106809ebe95bc13c2c821
Amicht <vicolicht@gmail.com>
2023-02-15 16:13:04 +0200

    tools update

---
+++ src/utils/tools.ts
@@ -0,0 +1,17 @@
+export const techTools = {
+    js:"JavaScript",
+    ts:"TypeScript", 
+    react: "ReactJS",
+    react_ts: "React-TypeScript",
+    angular: "AngularJS",
+    bootstrap: "Bootstrap",
+    sass: "SASS",
+    html: "HTML5",
+    css: "CSS3",
+    node: "NodeJS",
+    express: "ExpressJS",
+    mongodb: "MongoDB",
+    mysql: "MySql",
+    vanila_js: "VanillaJS",
+    rest_api: "Rest-api"
+}
