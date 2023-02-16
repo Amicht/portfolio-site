42e2d861908a7679c5c642eb88fe68f3ffcf5774
Amicht <vicolicht@gmail.com>
2023-02-16 15:01:37 +0200

    projectsFilterBar

--- src/components/my-projects/ProjectsFilterBar.tsx
+++ src/components/my-projects/ProjectsFilterBar.tsx
@@ -1,45 +1,47 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { projects } from '../../utils/projects';
-import { techTools } from '../../utils/tools';
+import { techTools } from '../../utils/techTools';
 import SkillBtn from './SkillBtn';
 
 interface Props{
     skillWordChangeHndler: (skill:string) => void;
+    activeSkill:string
 }
 
 interface SkillI{
     name:string,
-    match: number
+    match: number,
 }
-const ProjectsFilterBar = ({skillWordChangeHndler}:Props) => {
+const ProjectsFilterBar = ({skillWordChangeHndler,activeSkill}:Props) => {
 
     const [skills, setSkills] = React.useState<SkillI[]>([]);
+    const initSkills = [
+        techTools.js, techTools.react, techTools.angular, techTools.node, 
+        techTools.mysql, techTools.mongodb, techTools.ts, techTools.vanila_js, 
+    ]
 
     const allOptions = "All";
     const getSkillsWithProjectCount = () => {
-        const mappedSkills = Object.values(techTools).map((t,idx) => {
+        const mappedSkills = initSkills.map((t,idx) => {
             return {
                 name: t,
                 match: projects.filter((p,idx) => !!(p.tools.findIndex(skill => t === skill) !== -1)).length
             }
-        })
+        });
+        mappedSkills.unshift({name: allOptions, match: projects.length});
         setSkills(mappedSkills);
     }
 
     React.useEffect(() => { getSkillsWithProjectCount(); },[]);
 
   return (
-    <div className='d-flex row mt-3'>
-        <SkillBtn 
-            key={allOptions}
-            match={projects.length}
-            skillName={allOptions}
-            skillWordChangeHndler={skillWordChangeHndler}/>
+    <div className='row mt-3'>
 
         {skills.map((t,idx) => 
             <SkillBtn 
-                key={t.name}
+                key={t.name + idx}
                 skillName={t.name}
+                isActive={t.name === activeSkill}
                 match={t.match}
                 skillWordChangeHndler={skillWordChangeHndler} />
             )}
