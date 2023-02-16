b68c5bc10a9b41590abeefa80fc832b190077a14
Amicht <vicolicht@gmail.com>
2023-02-16 23:34:36 +0200

    margin issues fixed

--- src/components/my-projects/MyProjects.tsx
+++ src/components/my-projects/MyProjects.tsx
@@ -3,6 +3,7 @@ import { ProjectModel } from '../../models/ProjectModel';
 import { projects } from '../../utils/projects';
 import ProjectCard from '../ProjectCard';
 import ProjectsFilterBar from './ProjectsFilterBar';
+import './my-projects-section.scss';
 
 const MyProjects = () => {
 
@@ -29,23 +30,28 @@ const MyProjects = () => {
     }
 
   return (
-    <div>
-        <div className='text-center my-4'>
-          <h2 className='fs-1'>{txts.title}</h2>
-          <h3 className='fs-3'>{txts.subtitle}</h3>
-          <ProjectsFilterBar 
-            key={"filter-bar"}
-            skillWordChangeHndler={SkillWordChangeHndler}
-            activeSkill={activeSkill}/>
-        </div>
+    <section id="section-2" className='my-projects-sction py-5'>
+      <div className='container'>
+        <div data-aos="fade-up"
+            data-aos-duration="1200">
+            <div className='text-center my-4'>
+              <h2 className='fs-1'>{txts.title}</h2>
+              <h3 className='fs-3'>{txts.subtitle}</h3>
+              <ProjectsFilterBar 
+                key={"filter-bar"}
+                skillWordChangeHndler={SkillWordChangeHndler}
+                activeSkill={activeSkill}/>
+            </div>
 
-        <div className='row mt-5'>
-          {myProjects.map((p,idx) => 
-            <ProjectCard 
-              key={p.name + idx} 
-              {...p}/>)}
+            <div className='row mt-5'>
+              {myProjects.map((p,idx) => 
+                <ProjectCard 
+                key={p.name + idx} 
+                {...p}/>)}
+            </div>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
