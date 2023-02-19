5498a66abc8ce164c1241b69e83ab93f422bda72
Amicht <vicolicht@gmail.com>
2023-02-19 22:49:35 +0200

    with offcanvas navbar

---
+++ src/components/my-navbar/OffcanvasNavbar.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { ThemeCtxt } from '../../context/ThemeCtxt';
+import { navBarSections } from '../../utils/sections';
+import ContactComponent from '../contact-component/ContactComponent';
+import './costum-switch.scss';
+import './offcanvas-navbar.scss';
+import { Fade as Hamburger } from 'hamburger-react'
+
+interface Props{
+    chngeThemeHndler: () => void
+}
+
+
+const OffCanvasNavbar = ({chngeThemeHndler}:Props) => {
+    const {dark: {backgroundColor,color}} = React.useContext(ThemeCtxt);
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleClickScroll = (sectionId:string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <section style={{backgroundColor, color}} className="py-1 sticky-top">
+    <div className='container d-flex justify-content-end'>
+      <div onClick={handleShow} >
+        <Hamburger size={36} toggled={show}/>
+      </div>
+      <Offcanvas show={show} onHide={handleClose} style={{backgroundColor}}>
+        <Offcanvas.Header>
+            <i 
+              className="bi bi-x-lg close-icon" 
+              onClick={handleClose}
+              style={{color}}></i>
+            <label className="costum-switch my-4" >
+              <input  type="checkbox" onClick={chngeThemeHndler} />
+              <span style={{backgroundColor:color}} className="slider round"></span>
+            </label>
+        </Offcanvas.Header>
+        <Offcanvas.Body >
+
+        {navBarSections.map((sec,idx) => 
+            <div onClick={() =>{handleClickScroll(sec.id)}}
+                className='my-2 nav-title'
+              style={{color}}
+              key={sec.id} >
+                {sec.txt}
+            </div>)}
+            <div  className='d-flex container mt-4 ms-0 ps-0' >
+              <ContactComponent color={color} />
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+    </section>
+  );
+}
+
+
+export default OffCanvasNavbar;
